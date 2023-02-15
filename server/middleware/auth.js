8fa7ba33e3c44a065c5333a41cb302d27fb14e09
unknown <promotion.kang.jay@gmail.com>
2023-02-14 19:25:50 -0600

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+/* What logged in users can access */
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
+
+    if (token.startsWith("Bearer ")) { /* The token will placed after the bearer + space */
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
